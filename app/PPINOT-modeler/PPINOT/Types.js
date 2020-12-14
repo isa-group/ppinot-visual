4f1f58404319d894ef7471ac233179fd4eb48830
María Salas <maria.su.1997@hotmail.com>
2020-12-14 15:08:14 +0100

    Custom replacement by PPINOT

---
+++ app/PPINOT-modeler/PPINOT/Types.js
@@ -0,0 +1,306 @@
+export const label = [
+    'PPINOT:Resource',
+    'PPINOT:ResourceAbsence',
+    'PPINOT:Role',
+    'PPINOT:RoleAbsence',
+    'PPINOT:Group',
+    'PPINOT:GroupAbsence',
+    'PPINOT:Clock',
+    'PPINOT:TimeSlot',
+    'PPINOT:Avion',
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:CountMeasure',
+    'PPINOT:DataAggregatedMeasure',
+    'PPINOT:DataMeasure',
+    'PPINOT:DataPropertyConditionAggregatedMeasure',
+    'PPINOT:DataPropertyConditionMeasure',
+    'PPINOT:DerivedMultiInstanceMeasure',
+    'PPINOT:DerivedSingleInstanceMeasure',
+    'PPINOT:TimeMeasure',
+    'PPINOT:CyclicTimeMeasure',
+    'PPINOT:BaseMeasure',
+    'PPINOT:MyConnection',
+    'PPINOT:StartConnection',
+    'PPINOT:EndConnection',
+    'PPINOT:DashedLine',
+    'PPINOT:AggregatedConnection',
+    'PPINOT:GroupedBy',
+    'PPINOT:ToConnection',
+    'PPINOT:FromConnection',
+    'PPINOT:Ppi',
+    'PPINOT:StateConditionMeasure',
+    'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:Target',
+    'PPINOT:Scope',
+    'PPINOT:RFCStateConnection',
+    
+]
+
+export const externalLabel = [
+    'PPINOT:Resource',
+    'PPINOT:ResourceAbsence',
+    'PPINOT:Role',
+    'PPINOT:RoleAbsence',
+    'PPINOT:Group',
+    'PPINOT:GroupAbsence',
+    'PPINOT:Clock',
+    'PPINOT:Avion',
+    'PPINOT:Ppi',
+    'PPINOT:MyConnection',
+    'PPINOT:StartConnection',
+    'PPINOT:EndConnection',
+    'PPINOT:DashedLine',
+    'PPINOT:AggregatedConnection',
+    'PPINOT:GroupedBy',
+    'PPINOT:ToConnection',
+    'PPINOT:FromConnection',
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:CountMeasure',
+    //'PPINOT:DataAggregatedMeasure',
+    'PPINOT:DataMeasure',
+    'PPINOT:TimeMeasure',
+    'PPINOT:CyclicTimeMeasure',
+    'PPINOT:BaseMeasure',
+    // 'PPINOT:DataPropertyConditionAggregatedMeasure'
+    'PPINOT:StateConditionMeasure',
+    //'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:RFCStateConnection',
+
+]
+
+export const connections = [
+    'PPINOT:ResourceArc',
+    'PPINOT:ConsequenceFlow',
+    'PPINOT:TimeDistanceArcStart',
+    'PPINOT:TimeDistanceArcEnd',
+    'PPINOT:RFCStateConnection',
+    'PPINOT:MyConnection',
+    'PPINOT:DashedLine',
+    'PPINOT:AggregatedConnection',
+    'PPINOT:GroupedBy',
+    'PPINOT:ToConnection',
+    'PPINOT:FromConnection',
+    'PPINOT:StartConnection',
+    'PPINOT:EndConnection',    
+]
+
+export const directEdit = [
+    // 'PPINOT:Resource',
+    // 'PPINOT:Role',
+    // 'PPINOT:Group',
+    'PPINOT:Clock',
+    'PPINOT:TimeSlot',
+    'PPINOT:Avion',
+    'PPINOT:Target',
+    'PPINOT:Scope',
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:CountMeasure',
+    'PPINOT:DataAggregatedMeasure',
+    'PPINOT:DataMeasure',
+    'PPINOT:DataPropertyConditionAggregatedMeasure',
+    'PPINOT:DataPropertyConditionMeasure',
+    'PPINOT:DerivedMultiInstanceMeasure',
+    'PPINOT:DerivedSingleInstanceMeasure',
+    'PPINOT:TimeMeasure',
+    'PPINOT:CyclicTimeMeasure',
+    'PPINOT:Ppi',
+    'PPINOT:MyConnection',
+    'PPINOT:StartConnection',
+    'PPINOT:EndConnection',
+    'PPINOT:DashedLine',
+    'PPINOT:AggregatedConnection',
+    'PPINOT:GroupedBy',
+    'PPINOT:ToConnection',
+    'PPINOT:FromConnection',
+    'PPINOT:StateConditionMeasure',
+    'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:BaseMeasure',
+    'PPINOT:RFCStateConnection',
+]
+
+export const myConnectionElements = [
+    'PPINOT:Avion',
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:CountMeasure',
+    'PPINOT:DataAggregatedMeasure',
+    'PPINOT:DataMeasure',
+    'PPINOT:DataPropertyConditionAggregatedMeasure',
+    'PPINOT:DataPropertyConditionMeasure',
+    'PPINOT:DerivedMultiInstanceMeasure',
+    'PPINOT:DerivedSingleInstanceMeasure',
+    'PPINOT:Ppi',
+    'PPINOT:StateConditionMeasure',
+    'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:BaseMeasure'
+    
+]
+
+
+
+export const aggreagatedElements = [
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:DataAggregatedMeasure',
+    'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:DataPropertyConditionAggregatedMeasure'
+]
+
+export const timeMeasuresElements = [
+    //'PPINOT:TimeMeasure'  //no esta implementada
+]
+export const countMeasuresElements = [
+    'PPINOT:CountMeasure'  //no esta implementada
+]
+export const ppiElement = [
+    'PPINOT:Ppi',
+    // 'PPINOT:CountMeasure',
+    // 'PPINOT:DataMeasure'
+
+]
+export const resourceArcElements = [
+    'PPINOT:Clock',
+    'PPINOT:Resource',
+    'PPINOT:ResourceAbsence',
+    'PPINOT:Role',
+    'PPINOT:RoleAbsence',
+    'PPINOT:Group',
+    'PPINOT:GroupAbsence',
+    'PPINOT:TimeSlot',
+    'PPINOT:Avion',
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:CountMeasure',
+    'PPINOT:DataAggregatedMeasure',
+    'PPINOT:DataMeasure',
+    'PPINOT:DataPropertyConditionAggregatedMeasure',
+    'PPINOT:DataPropertyConditionMeasure',
+    'PPINOT:DerivedMultiInstanceMeasure',
+    'PPINOT:DerivedSingleInstanceMeasure',
+    'PPINOT:TimeMeasure',
+    'PPINOT:CyclicTimeMeasure',
+    'PPINOT:StateConditionMeasure',
+    'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:BaseMeasure'
+]
+
+export const PPINOT = [
+    'PPINOT:Clock',
+    'PPINOT:Resource',
+    'PPINOT:ResourceAbsence',
+    'PPINOT:Role',
+    'PPINOT:RoleAbsence',
+    'PPINOT:Group',
+    'PPINOT:GroupAbsence',
+    'PPINOT:TimeSlot',
+    'PPINOT:ResourceArc',
+    'PPINOT:ConsequenceFlow',
+    'PPINOT:TimeDistanceArcStart',
+    'PPINOT:TimeDistanceArcEnd',
+    'PPINOT:Avion',
+    'PPINOT:AggregatedMeasure',
+    'PPINOT:TimeAggregatedMeasure',
+    'PPINOT:CyclicTimeAggregatedMeasure',
+    'PPINOT:CountAggregatedMeasure',
+    'PPINOT:CountMeasure',
+    'PPINOT:DataAggregatedMeasure',
+    'PPINOT:DataMeasure',
+    'PPINOT:DataPropertyConditionAggregatedMeasure',
+    'PPINOT:DataPropertyConditionMeasure',
+    'PPINOT:DerivedMultiInstanceMeasure',
+    'PPINOT:DerivedSingleInstanceMeasure',
+    'PPINOT:TimeMeasure',
+    'PPINOT:CyclicTimeMeasure',
+    'PPINOT:BaseMeasure',
+    'PPINOT:MyConnection',
+    'PPINOT:StartConnection',
+    'PPINOT:EndConnection',
+    'PPINOT:DashedLine',
+    'PPINOT:AggregatedConnection',
+    'PPINOT:GroupedBy',
+    'PPINOT:ToConnection',
+    'PPINOT:FromConnection',
+    'PPINOT:Ppi',
+    'PPINOT:StateConditionMeasure',
+    'PPINOT:StateConditionAggregatedMeasure',
+    'PPINOT:RFCStateConnection'
+
+    
+]
+
+export function isExternalLabel(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return externalLabel.includes(type)
+}
+
+export function isPPINOTShape(type) {
+    if (typeof type === 'object')
+        type = type.type
+
+    return type.includes('PPINOT:') && !connections.includes(type)
+}
+
+export function isPPINOTConnection(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return type.includes('PPINOT:') && connections.includes(type)
+}
+
+export function isPPINOTResourceArcElement(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return resourceArcElements.includes(type)
+}
+
+export function isPPINOTMyConnectionElement(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return myConnectionElements.includes(type)
+}
+
+export function isPPINOTAggregatedElement(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return aggreagatedElements.includes(type)
+}
+
+export function isPPINOTTimeMeasureElement(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return timeMeasuresElements.includes(type)
+}
+export function isPPINOTCountMeasureElement(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return countMeasuresElements.includes(type)
+}
+
+export function isPPINOTPpi(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return ppiElement.includes(type)
+}
